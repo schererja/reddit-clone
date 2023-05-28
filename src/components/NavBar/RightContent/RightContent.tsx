b8979c65e1528859cf4ce6cecb4550cb6add5266
Jason Scherer <schereja@gmail.com>
2023-05-27 19:28:38 -0500

    initial commit

---
+++ src/components/NavBar/RightContent/RightContent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type RightContentProps = {
+  // user: any;
+};
+
+const RightContent: React.FC<RightContentProps> = () => {
+  return (
+    <>
+      <AuthModel></AuthModel>
+    </>
+  );
+};
+export default RightContent;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
+}
